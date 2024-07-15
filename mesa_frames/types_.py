a9c9925e56567d4388896b17df881b5f5563f585
Adam Amer <136176500+adamamer20@users.noreply.github.com>
2024-07-16 00:23:43 +0200

    removing space types (has it's own PR)

--- mesa_frames/types_.py
+++ mesa_frames/types_.py
@@ -1,12 +1,9 @@
 from collections.abc import Collection
+from typing import Literal
 
-import geopandas as gpd
-import geopolars as gpl
-import numpy as np
 import pandas as pd
 import polars as pl
 from numpy import ndarray
-from typing import Literal, Sequence
 
 ####----- Agnostic Types -----####
 AgnosticMask = Literal["all", "active"] | None
@@ -18,20 +15,16 @@ ArrayLike = pd.api.extensions.ExtensionArray | ndarray
 AnyArrayLike = ArrayLike | pd.Index | pd.Series
 PandasMaskLike = AgnosticMask | pd.Series | pd.DataFrame | AnyArrayLike
 PandasIdsLike = AgnosticIds | pd.Series | pd.Index
-PandasGridCapacity = np.ndarray
 
 ###----- Polars Types -----###
 
 PolarsMaskLike = AgnosticMask | pl.Expr | pl.Series | pl.DataFrame | Collection[int]
 PolarsIdsLike = AgnosticIds | pl.Series
-PolarsGridCapacity = list[pl.Expr]
 
 ###----- Generic -----###
 
-GeoDataFrame = gpd.GeoDataFrame | gpl.GeoDataFrame
-GeoSeries = gpd.GeoSeries | gpl.GeoSeries
 DataFrame = pd.DataFrame | pl.DataFrame
-Series = pd.Series | pl.Series | GeoSeries
+Series = pd.Series | pl.Series
 Index = pd.Index | pl.Series
 BoolSeries = pd.Series | pl.Series
 MaskLike = AgnosticMask | PandasMaskLike | PolarsMaskLike
@@ -39,35 +32,3 @@ IdsLike = AgnosticIds | PandasIdsLike | PolarsIdsLike
 
 ###----- Time ------###
 TimeT = float | int
-
-
-###----- Space -----###
-
-NetworkCoordinate = int | DataFrame
-
-GridCoordinate = int | Sequence[int] | DataFrame
-
-DiscreteCoordinate = NetworkCoordinate | GridCoordinate
-ContinousCoordinate = float | Sequence[float] | DataFrame
-
-SpaceCoordinate = DiscreteCoordinate | ContinousCoordinate
-
-
-NetworkCoordinates = NetworkCoordinate | Collection[NetworkCoordinate]
-GridCoordinates = (
-    GridCoordinate | Sequence[int | slice | Sequence[int]] | Collection[GridCoordinate]
-)
-
-DiscreteCoordinates = NetworkCoordinates | GridCoordinates
-ContinousCoordinates = (
-    ContinousCoordinate
-    | Sequence[float | Sequence[float]]
-    | Collection[ContinousCoordinate]
-)
-
-SpaceCoordinates = DiscreteCoordinates | ContinousCoordinates
-
-GridCapacity = PandasGridCapacity | PolarsGridCapacity
-NetworkCapacity = DataFrame
-
-DiscreteSpaceCapacity = GridCapacity | NetworkCapacity
