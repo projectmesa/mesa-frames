46500a736a2f4393b2b3c332f8eefee73117a513
Adam Amer <136176500+adamamer20@users.noreply.github.com>
2024-07-25 18:42:07 +0200

    adding GridPandas to __init__

--- mesa_frames/__init__.py
+++ mesa_frames/__init__.py
@@ -1,11 +1,13 @@
 from mesa_frames.concrete.agents import AgentsDF
-from mesa_frames.concrete.pandas.agentset import AgentSetPandas
-from mesa_frames.concrete.polars.agentset import AgentSetPolars
 from mesa_frames.concrete.model import ModelDF
+from mesa_frames.concrete.pandas.agentset import AgentSetPandas
+from mesa_frames.concrete.pandas.space import GridPandas
+from mesa_frames.concrete.polars.agentset import AgentSetPolars
 
 __all__ = [
     "AgentsDF",
     "AgentSetPandas",
     "AgentSetPolars",
     "ModelDF",
+    "GridPandas",
 ]
