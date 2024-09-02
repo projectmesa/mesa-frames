cd78529ce4ff9fa7a2248442227cc8921bab2eec
Adam Amer <136176500+adamamer20@users.noreply.github.com>
2024-09-02 15:20:08 +0200

    fix: change Author list to Author str

--- docs/api/conf.py
+++ docs/api/conf.py
@@ -10,7 +10,7 @@ sys.path.insert(0, str(Path("..").resolve()))
 
 # -- Project information -----------------------------------------------------
 project = "mesa-frames"
-author = ["Project Mesa", "Adam Amer"]
+author = "Project Mesa, Adam Amer"
 copyright = f"2023, {author}"
 
 # -- General configuration ---------------------------------------------------
