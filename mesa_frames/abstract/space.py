ff1c3875b760382a1ee685dd99b15078472ac27c
Adam Amer <136176500+adamamer20@users.noreply.github.com>
2025-04-19 17:40:55 +0200

    removing pandas import (outdated)

--- mesa_frames/abstract/space.py
+++ mesa_frames/abstract/space.py
@@ -54,7 +54,6 @@ from typing import TYPE_CHECKING, Literal
 from warnings import warn
 
 import numpy as np
-import pandas as pd
 import polars as pl
 from numpy.random import Generator
 from typing_extensions import Any, Self
