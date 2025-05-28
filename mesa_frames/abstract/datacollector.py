2a9ef6918aa4843a07655423561db0046de61f89
Ben <bengeoabraham@gmail.com>
2025-05-29 01:47:48 +0530

    fleshed out flush

--- mesa_frames/abstract/datacollector.py
+++ mesa_frames/abstract/datacollector.py
@@ -51,17 +51,39 @@ from typing import Callable, Dict, Optional, Union, Any
 from agents import ModelDF
 
 class AbstractDataCollector(ABC):
+    """
+    Abstract Base Class for Mesa-Frames DataCollector.
+
+    This class defines methods for collecting data from both model and agents.
+    Sub classes must implement logic for the methods
+    """
 
     _model: ModelDF
 
     def __init__(
         self,
-        model: Any,
-        model_reporters: Optional[Dict[str, Union[str, Callable]]] = None,
+        model: ModelDF,
+        model_reporters: Optional[Dict[str, Callable]] = None,
         agent_reporters: Optional[Dict[str, Union[str, Callable]]] = None,
         trigger: Optional[Callable[[Any], bool]] = None,
         storage: Optional[str] = None
     ):
+        """
+        Initialize a Datacollector
+        
+        Parameters
+        ----------
+        model : ModelDF
+            The model objet from which data is collected
+        model_reporters: Dict[str, Callable], Optional
+            functions to collect data at model level
+        agent_reporters: Dict[str, Union[str, Callable]], Optional
+            functions or attribute name to collect data at agent level
+        trigger: Callable, optional
+            a function(model) that returns a bool, default always collects
+        storage: Optional[str] = None
+            Storage backend URI (e.g. 'memory:','postgressql://...',etc.), default is to store in memory
+        """
         self._model = model 
         self._model_reporters = model_reporters or {}
         self._agent_reporters = agent_reporters or {}
@@ -70,25 +92,33 @@ class AbstractDataCollector(ABC):
         self._frames = [] 
 
     def collect(self) -> None:
-        """ collect data - doesn't materialise until called"""
+        """ Trigger data collection if condition is met"""
         if self.should_collect():
             self._collect() 
 
-    @abstractmethod
-    def _collect(self):
-        """ actual collection logic"""
-        pass
-
     def should_collect(self) -> bool:
-        """trigger == True"""
+        """ Evaluates whether data should be collected or not"""
         return self._trigger(self._model)
 
     @abstractmethod
-    def get_data(self) -> Any:
-        """ returns collected data currently in memory"""
+    def _collect(self):
+        """ performs actual data collection"""
         pass
 
     @abstractmethod
+    def get_data(self) -> Any:
+        """ returns collected data currently in memory as a dataframe"""
+        pass
+
+    def flush(self) ->None:
+        """public method to flush the collected data and reset frames"""
+        self._flush()
+        self.reset()
+    def reset(self):
+        """ method to reset the data in memory"""
+        self._frames = []
+
+    @abstractmethod
     def _flush(self) -> None:
-        """if configured persists to external"""
+        """persists collected data to external storage and"""
         pass
