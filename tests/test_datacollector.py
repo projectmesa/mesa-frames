1f2e68d935c080a37f5fedd06fbf103bd6d8c48c
Ben <bengeoabraham@gmail.com>
2025-06-22 03:39:19 +0530

    test model

--- tests/test_datacollector.py
+++ tests/test_datacollector.py
@@ -1,5 +1,5 @@
 from mesa_frames.concrete.datacollector import DataCollector
-from mesa_frames import ModelDF,AgentSetPolars
+from mesa_frames import ModelDF,AgentSetPolars,AgentsDF
 import pytest
 import polars as pl
 
@@ -7,21 +7,11 @@ import polars as pl
 def custom_trigger(model):
     return True
 
-
 class ExampleAgentSetPolars(AgentSetPolars):
     def __init__(self, model: ModelDF):
         super().__init__(model)
-        self.starting_wealth = pl.Series("wealth", [1, 2, 3, 4])
-        self.dc = DataCollector(
-            model=model,
-            trigger=custom_trigger,
-            model_reporters={
-                "total_agents": lambda model: len(model._agents._agentsets[0])
-            },
-            agent_reporters={
-                "wealth": lambda model: model._agents._agentsets[0]["wealth"]
-            }
-        )
+        self["wealth"] = pl.Series("wealth", [1, 2, 3, 4])
+        self["age"] = pl.Series("age", [10, 20, 30, 40])
 
     def add_wealth(self, amount: int) -> None:
         self["wealth"] += amount
@@ -29,25 +19,90 @@ class ExampleAgentSetPolars(AgentSetPolars):
     def step(self) -> None:
         self.add_wealth(1)
 
+
+class ExampleModel(ModelDF):
+    def __init__(self, agents: AgentsDF):
+        super().__init__()
+        self.agents = agents
+        self.dc = DataCollector(
+            model=self,
+            trigger=custom_trigger,
+            model_reporters={
+                "total_agents": lambda model: sum(len(agentset) for agentset in model.agents._agentsets)
+            }
+        )
+
+    def step(self):
+        self.agents.do("step")
+
+    def run_model(self, n):
+        for _ in range(n):
+            self.step()
+
 @pytest.fixture
 def fix1_AgentSetPolars() -> ExampleAgentSetPolars:
-    model = ModelDF()
-    agents = ExampleAgentSetPolars(model)
-    agents["wealth"] = agents.starting_wealth
-    agents["age"] = [10, 20, 30, 40]
-    model.agents.add(agents)
-    return agents
+    return ExampleAgentSetPolars(ModelDF())
+
 @pytest.fixture
-def fix3_AgentSetPolars() -> ExampleAgentSetPolars:
+def fix_AgentsDF(
+    fix1_AgentSetPolars: ExampleAgentSetPolars
+) -> AgentsDF:
     model = ModelDF()
-    agents = ExampleAgentSetPolars(model)
-    agents["wealth"] = agents.starting_wealth + 7
-    agents["age"] = [12, 13, 14, 116]
+    agents = AgentsDF(model)
+    agents.add([fix1_AgentSetPolars])
     return agents
 
+@pytest.fixture
+def fix1_model(fix_AgentsDF: AgentsDF) -> ExampleModel:
+    return ExampleModel(fix_AgentsDF)
+
+
 class Test_DataCollector:
-    def test_model(self,fix1_AgentSetPolars):
-        agents = fix1_AgentSetPolars
-        assert agents["wealth"] == [1,2,3,4]
-        assert agents["wealth"] == [1,5,3,4]
-        
+    def test_collect(self,fix1_model):
+        model = fix1_model
+
+
+        model.dc.collect()
+        collected_data = model.dc.data
+
+        #test collected_model_data
+        print(collected_data)
+        assert collected_data["model"]["step"].to_list() == [0]
+        assert collected_data["model"]["total_agents"].to_list() == [4]
+        with pytest.raises(
+            pl.exceptions.ColumnNotFoundError, match='max_wealth'
+        ):
+             collected_data["model"]["max_wealth"]
+
+
+        # fails agent level due to agentsdf
+         
+        # test collected_agent_data
+        # assert collected_data["agent"]["step"].to_list() == [0,0,0,0]
+        # assert collected_data["agent"]["wealth"].to_list() == [1,2,3,4]
+        # with pytest.raises(
+        #     pl.exceptions.ColumnNotFoundError, match='max_wealth'
+        # ):
+        #      collected_data["agent"]["max_wealth"]
+
+    def test_collect_step(self,fix1_model):
+        agents = fix1_model.agents
+
+        #base check
+        model = fix1_model
+        model.run_model(5)
+
+        model.dc.collect()
+        collected_data = model.dc.data
+
+        #test collected_model_data
+        print(collected_data)
+        assert collected_data["model"]["step"].to_list() == [5]
+        assert collected_data["model"]["total_agents"].to_list() == [4]
+        with pytest.raises(
+            pl.exceptions.ColumnNotFoundError, match='max_wealth'
+        ):
+             collected_data["model"]["max_wealth"]
+
+             
+        # fails agent level due to agentsdf
